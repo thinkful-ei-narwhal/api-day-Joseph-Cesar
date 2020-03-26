c963fd40a11b9066687e47333a32301c30a13fc2
Joseph Rivera <josephr98@gmail.com>
2020-03-26 11:10:04 -0400

    finished drill 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import $ from "jquery";
-
 import shoppingList from "./shopping-list";
 import store from "./store";
 import item from "./item";
+import api from './api';
 
 import "./index.css";
 
 const main = function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems()
+    .then(res => console.log(res));
 };
 
 $(main);
