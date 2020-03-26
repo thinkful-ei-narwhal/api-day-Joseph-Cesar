73859f089a15f3986dcec2aaac3f811e5dddfc42
Joseph Rivera <josephr98@gmail.com>
2020-03-26 12:01:02 -0400

    completed drill 4

--- src/api.js
+++ src/api.js
@@ -2,8 +2,22 @@ const BASE_URL = 'https://thinkful-list-api.herokuapp.com/joseph';
 
 function getItems() {
   return fetch(`${BASE_URL}/items`)
-}
+};
+
+function createItem(name) {
+  const newItem = {
+    name,
+  }
+  let secondArg = {
+    method: 'POST', 
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(newItem) 
+  }
+
+  return fetch(`${BASE_URL}/items`, secondArg);
+};
 
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import api from './api';
 import "./index.css";
 
 const main = function() {
+  api.createItem('pears')
+  .then(res => res.json())
+  .then((newItem) => {
+    return api.getItems();
+  })
+  .then(res => res.json())
+  .then((items) => {
+    console.log(items);
+  });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
   api.getItems()
